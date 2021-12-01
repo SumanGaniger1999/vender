7030bfee462c92a84b7c14160952e9860b22dd34
SumanGaniger1999 <89911110+SumanGaniger1999@users.noreply.github.com>
2021-12-01 22:57:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,198 @@
+ function showRoom(name){
+               
+                if(name=="Lamborgini")
+                {
+                    var rate=60000000;
+                    var quantity=document.getElementById("name").value;
+                var total=quantity*rate;
+                document.getElementById("print").innerHTML=total;
+
+                    
+                }
+                if(name=="SUV")
+                {
+                    var rate=2000000;
+                    var quantity=document.getElementById("name1").value;
+                var total=quantity*rate;
+                document.getElementById("print1").innerHTML=total;
+                    
+                }
+                if(name=="Audi A3")
+                {
+                    var rate=1000000;
+                    var quantity=document.getElementById("name2").value;
+                var total=quantity*rate;
+                document.getElementById("print2").innerHTML=total;
+                    
+                }
+                if(name=="Datsun-go1")
+                {
+                    var rate=1200000;
+                    var quantity=document.getElementById("name3").value;
+                var total=quantity*rate;
+                document.getElementById("print3").innerHTML=total;
+                    
+                }
+                if(name=="Hyundai Venue")
+                {
+                    var rate=1100000;
+                    var quantity=document.getElementById("name4").value;
+                var total=quantity*rate;
+                document.getElementById("print4").innerHTML=total;
+                    
+                }
+                if(name=="Mercedes Benz")
+                {
+                    var rate=20000000;
+                    var quantity=document.getElementById("name5").value;
+                var total=quantity*rate;
+                document.getElementById("print5").innerHTML=total;
+                    
+                }
+                if(name=="Bajaj Pulsar")
+                {
+                    var rate=200000;
+                    var quantity=document.getElementById("name6").value;
+                var total=quantity*rate;
+                document.getElementById("print6").innerHTML=total;
+                    alert("The price is:2 lakh ")
+                }
+                if(name=="Kawasaki Z650RS")
+                {
+                    var rate=600000;
+                    var quantity=document.getElementById("name7").value;
+                var total=quantity*rate;
+                document.getElementById("print7").innerHTML=total;
+                    alert("The price is:6 lakh ")
+                }
+                if(name==" DucatiHypermotard")
+                {
+                    var rate=1600000;
+                    var quantity=document.getElementById("name8").value;
+                var total=quantity*rate;
+                document.getElementById("print8").innerHTML=total;
+                   
+                }
+                if(name=="KTM RC200")
+                {
+                    var rate=300000;
+                    var quantity=document.getElementById("name9").value;
+                var total=quantity*rate;
+                document.getElementById("print9").innerHTML=total;
+                  
+                }
+                if(name=="Hyabusa")
+                {
+                    var rate=800000;
+                    var quantity=document.getElementById("name10").value;
+                var total=quantity*rate;
+                document.getElementById("print10").innerHTML=total;
+                   
+                }
+                if(name=="Tvs-xl-Heavyduty")
+                {
+                    var rate=50000;
+                    var quantity=document.getElementById("name11").value;
+                var total=quantity*rate;
+                document.getElementById("print11").innerHTML=total;
+        
+                }
+                if(name=="Cd-100-ss")
+                {
+                    var rate=60000;
+                    var quantity=document.getElementById("name12").value;
+                var total=quantity*rate;
+                document.getElementById("print12").innerHTML=total;
+                    
+                }
+                if(name=="Yezdi")
+                {
+                    var rate=90000;
+                    var quantity=document.getElementById("name13").value;
+                var total=quantity*rate;
+                document.getElementById("print13").innerHTML=total;
+                    
+                }
+                if(name=="Jawa Perak")
+                {
+                    var rate=300000;
+                    var quantity=document.getElementById("name14").value;
+                var total=quantity*rate;
+                document.getElementById("print14").innerHTML=total;
+                    
+                }
+                if(name=="Hf Delux")
+                {
+                    var rate=90000;
+                    var quantity=document.getElementById("name15").value;
+                var total=quantity*rate;
+                document.getElementById("print15").innerHTML=total;
+                    alert("The price is:90000 ")
+                }
+                if(name=="BMW")
+                {
+                    var rate=8000000;
+                    var quantity=document.getElementById("name16").value;
+                var total=quantity*rate;
+                document.getElementById("print16").innerHTML=total;
+                    
+                }
+                if(name=="DAO Vidyut")
+                {
+                    var rate=63000;
+                    var quantity=document.getElementById("name17").value;
+                var total=quantity*rate;
+                document.getElementById("print17").innerHTML=total;
+                    
+                }
+                if(name=="Hindustan")
+                {
+                    var rate=1000000;
+                    var quantity=document.getElementById("name18").value;
+                var total=quantity*rate;
+                document.getElementById("print18").innerHTML=total;
+                    
+                }
+                if(name=="HMT")
+                {
+                    var rate=200000;
+                    var quantity=document.getElementById("name19").value;
+                var total=quantity*rate;
+                document.getElementById("print19").innerHTML=total;
+                    
+                }
+                if(name=="JCB")
+                {
+                    var rate=3000000;
+                    var quantity=document.getElementById("name20").value;
+                var total=quantity*rate;
+                document.getElementById("print20").innerHTML=total;
+                    
+                }
+                if(name=="Van")
+                {
+                    var rate=200000;
+                    var quantity=document.getElementById("name21").value;
+                var total=quantity*rate;
+                document.getElementById("print21").innerHTML=total;
+                   
+                }
+                if(name=="Bolero")
+                {
+                    var rate=400000;
+                    var quantity=document.getElementById("name22").value;
+                var total=quantity*rate;
+                document.getElementById("print22").innerHTML=total;
+                    
+                }
+                if(name=="Auto Ricksha")
+                {
+                    var rate=100000;
+                    var quantity=document.getElementById("name23").value;
+                var total=quantity*rate;
+                document.getElementById("print23").innerHTML=total;
+                   
+                }
+               
+
+            }
